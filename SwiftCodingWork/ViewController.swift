366a343b16ab0473b74ec8cb7e82022e68c4211b
ChrisR <CRhode@aol.com>
2020-04-23 14:59:09 -0700

    Initial Commit

---
+++ SwiftCodingWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCodingWork
+//
+//  Created by Christopher Rhode on 4/23/20.
+//  Copyright © 2020 Christopher Rhode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
